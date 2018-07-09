52c2ea55f0aac3ae078830d3149ee07ff5c7c45b
himanoa <matsunoappy@gmail.com>
2018-07-10 05:09:16 +0900

    feat: Mentionのみに反応するように修正

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 import discord
 from dotenv import load_dotenv, find_dotenv
-load_dotenv(find_dotenv())
 from os import getenv
+from re import sub
+
+load_dotenv(find_dotenv())
 
 client = discord.Client()
 
+
 @client.event
 async def on_message(message):
-    if message.author != client.user:
-        await client.send_message(message.channel, message.content)
+    if [user for user in message.mentions if user == client.user]:
+        removed_mention_content = (sub(r'<.+>\s', '', message.content))
+        if message.author != client.user:
+            await client.send_message(message.channel, removed_mention_content)
 
 client.run(getenv('DISCORD_TOKEN'))
