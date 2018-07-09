8b8c29dd99452e9d1486a8a33a01e8a835309e86
himanoa <matsunoappy@gmail.com>
2018-07-10 03:56:39 +0900

    add: オウムになるまでに成長したDiscordBotちゃん

--- main.py
+++ main.py
@@ -1,24 +1,13 @@
-from requests import get
+import discord
 from dotenv import load_dotenv, find_dotenv
-from os import getenv
-from pprint import pprint
-
 load_dotenv(find_dotenv())
+from os import getenv
 
-api_url = 'https://www.googleapis.com/youtube/v3/search'
+client = discord.Client()
 
+@client.event
+async def on_message(message):
+    if message.author != client.user:
+        await client.send_message(message.channel, message.content)
 
-def build_params(channel_id, key=getenv('YOUTUBE_TOKEN')):
-    return {
-        'part': 'snippet',
-        'channelId': channel_id,
-        'type': 'video',
-        'eventType': 'live',
-        'key': key
-    }
-
-
-lives = (get(api_url, params=build_params('UCdn5BQ06XqgXoAxIhbqw5Rg')).json()
-         .get('items'))
-
-pprint(lives)
+client.run(getenv('DISCORD_TOKEN'))
