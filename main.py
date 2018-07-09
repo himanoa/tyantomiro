8c0292d525a181af9299b9fba2f121314cb66aa6
himanoa <matsunoappy@gmail.com>
2018-07-10 06:32:40 +0900

    feat: VCに参加させる

--- main.py
+++ main.py
@@ -12,7 +12,11 @@ client = discord.Client()
 async def on_message(message):
     if [user for user in message.mentions if user == client.user]:
         removed_mention_content = (sub(r'<.+>\s', '', message.content))
-        if message.author != client.user:
-            await client.send_message(message.channel, removed_mention_content)
+        if removed_mention_content.startswith('joinvc'):
+            if message.author.voice_channel is None:
+                await client.send_message(message.channel, '先にVCに入ってくれ')
+            vc = client.get_channel(message.author.voice_channel.id)
+            await client.join_voice_channel(vc)
+
 
 client.run(getenv('DISCORD_TOKEN'))
