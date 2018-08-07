e1b55adeedfc713d9beeeffc6b23972ca56342e7
himanoa <matsunoappy@gmail.com>
2018-08-08 02:56:44 +0900

    test: Add help response test

---
+++ test/responses/test_help_response.py
@@ -0,0 +1,45 @@
+import unittest
+import asyncio
+
+from tyantomiro.responses.help_response import HelpResponse
+from tyantomiro.responses.base_response import BaseResponse
+
+
+class StubMessage:
+    def __init__(self):
+        self.channel = None
+
+
+class StubClient:
+    def __init__(self):
+        self.client = None
+
+    async def send_message(self, channel, help):
+        pass
+
+
+class TestHelpResponse(unittest.TestCase):
+
+    def test_should_be_return_empty_string(self):
+        expected = '\n'
+        self.assertEqual(HelpResponse([]).create_help(StubClient()), expected)
+
+    def test_should_be_run_execute(self):
+        event_loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(event_loop)
+
+        async def run_test():
+            expected = ''
+            await HelpResponse([]).execute(
+                '',
+                StubMessage(),
+                StubClient()
+            ), expected
+
+        coro = asyncio.coroutine(run_test)
+        event_loop.run_until_complete(coro())
+        event_loop.close()
+
+
+if __name__ == '__main__':
+    unittest.main()
--- tyantomiro/responses/help_response.py
+++ tyantomiro/responses/help_response.py
@@ -1,14 +1,17 @@
 from .base_response import BaseResponse
 
 class HelpResponse(BaseResponse):
+
     def __init__(self, responses):
         self.responses = responses
 
     async def execute(self, pattern, message, client):
-        help = '''\
-        {}
-        '''.format("\n".join([r.help(client) for r in self.responses]))
-        await client.send_message(message.channel, help)
+        await client.send_message(message.channel, self.create_help(client))
+
+    def create_help(self, client):
+        return '''\
+{}
+'''.format("\n".join([r.help(client) for r in self.responses]))
 
     def help(self, client):
         return ""
