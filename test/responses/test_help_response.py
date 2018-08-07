19e95d433e3ba74c635217540a92a8e1bdcabc36
himanoa <matsunoappy@gmail.com>
2018-08-08 03:00:52 +0900

    fix: Update gitignore

--- test/responses/test_help_response.py
+++ test/responses/test_help_response.py
@@ -24,6 +24,21 @@ class TestHelpResponse(unittest.TestCase):
         expected = '\n'
         self.assertEqual(HelpResponse([]).create_help(StubClient()), expected)
 
+    def test_should_be_return_help_response(self):
+        expected = "foobar\n"
+
+        class StubClient:
+            def __init__(self):
+                self.user = 'test#1234'
+
+        class StubResponse(BaseResponse):
+            def help(self, client):
+                return "foobar"
+
+        self.assertEqual(
+            HelpResponse([StubResponse()]).create_help(StubClient()), expected
+        )
+
     def test_should_be_run_execute(self):
         event_loop = asyncio.new_event_loop()
         asyncio.set_event_loop(event_loop)
